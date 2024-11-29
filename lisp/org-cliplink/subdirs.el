92ac7fc2f237c38c3f5d19a154eb37caee3b6d9b
Tara Druffel <taradruffel@outlook.com>
2024-11-29 16:57:06 +0100

    something

--- lisp/org-cliplink/subdirs.el
+++ lisp/org-cliplink/subdirs.el
@@ -1,6 +1,6 @@
 ;; In load-path, after this directory should come  -*- lexical-binding: t -*-
 ;; certain of its subdirectories.  Here we specify them.
-(normal-top-level-add-to-load-path '("test-data" "test" "request_handlers" ))
+(normal-top-level-add-to-load-path '("request_handlers" ))
 ;; Local Variables:
 ;; version-control: never
 ;; no-byte-compile: t
